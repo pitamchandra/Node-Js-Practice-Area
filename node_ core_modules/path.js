9d758cc94af4014b06d6bf4acd51fe9c47232bba
pitamchandra <pitamchandra402@gmail.com>
2025-03-09 13:49:56 +0600

    node core module path

---
+++ node_ core_modules/path.js
@@ -0,0 +1,9 @@
+let path = require('path');
+
+let myPath = "D:/r2ait mern/Node Js Practice Area/node_ core_modules/path.js"
+
+// console.log(path.basename(myPath));
+// console.log(path.dirname(myPath));
+// console.log(path.extname(myPath));
+// console.log(path.delimiter);
+console.log(path.format({dir: "D:/reait mern/node", base: "lala.js"}));
