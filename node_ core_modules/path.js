3db814451b91e994da7f85ad155e27a91407a036
pitamchandra <pitamchandra402@gmail.com>
2025-03-09 15:26:07 +0600

    path complete

--- node_ core_modules/path.js
+++ node_ core_modules/path.js
@@ -6,4 +6,9 @@ let myPath = "D:/r2ait mern/Node Js Practice Area/node_ core_modules/path.js"
 // console.log(path.dirname(myPath));
 // console.log(path.extname(myPath));
 // console.log(path.delimiter);
-console.log(path.format({dir: "D:/reait mern/node", base: "lala.js"}));
+// console.log(path.format({dir: "D:/reait mern/node", base: "lala.js"}));
+
+// console.log(path.isAbsolute(myPath));
+// console.log(path.join('Users', 'Refsnes', '..', 'demo_path.js'));
+// console.log(path.normalize('Users/Refsnes/../Jackson'));
+console.log(path.parse(myPath).base);
